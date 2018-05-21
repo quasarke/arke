0096fa853763bc2768b4f595d963f303556b4927
Zachary Way <zway@paytel.com>
2018-05-21 15:57:24 -0400

    removed unnecessary properties from the init step

--- Arke.Steps/ArkeInitStep/ArkeInitStepSettings.cs
+++ Arke.Steps/ArkeInitStep/ArkeInitStepSettings.cs
@@ -6,13 +6,6 @@ namespace Arke.Steps.ArkeInitStep
 {
     public class ArkeInitStepSettings : NodeProperties
     {
-        public string DeviceConfigEndpoint { get; set; }
-
-        public override NodeProperties ConvertFromJObject(JObject jObject)
-        {
-            base.ConvertFromJObject(jObject);
-            DeviceConfigEndpoint = jObject.GetValue("DeviceConfigEndpoint").Value<string>();
-            return this;
-        }
+        
     }
 }
---
+++ Arke.Steps/CallLoopStep/CallLoopStepSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Arke.DSL.Step;
+
+namespace Arke.Steps.CallLoopStep
+{
+    public class CallLoopStepSettings : NodeProperties
+    {
+    }
+}
