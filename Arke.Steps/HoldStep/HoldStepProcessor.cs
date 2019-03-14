9aee636a10a91fb4bcf0f8075c181eb193f57956
Zachary Way <zway@paytel.com>
2019-03-14 16:49:27 -0400

    Fixing some hanging issues with the Hold Step

--- Arke.Steps/HoldStep/HoldStepProcessor.cs
+++ Arke.Steps/HoldStep/HoldStepProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Timers;
 using Arke.DSL.Step;
 using Arke.SipEngine.Api;
 using Arke.SipEngine.Bridging;
@@ -21,6 +22,7 @@ namespace Arke.Steps.HoldStep
         public Dictionary<string, string> LogData = new Dictionary<string, string>();
         public string Name => "HoldStep";
         private IBridge _holdingBridge;
+        private int _timeoutStep = 0;
         
         public async Task DoStep(Step step, ICall call)
         {
@@ -28,6 +30,8 @@ namespace Arke.Steps.HoldStep
             _call.OnWorkflowStep += OnWorkflowStep;
             _call.Logger.Information("Hold processor start {@Call}", call.CallState);
             _settings = (HoldStepSettings)step.NodeData.Properties;
+            _timeoutStep = step.GetStepFromConnector("TimeoutStep");
+            
             _holdingBridge = await call.CreateBridge(BridgeType.Holding);
             _call.CallState.SetBridge(_holdingBridge);
             await _call.SipBridgingApi.AddLineToBridge(_call.CallState.GetIncomingLineId(), _call.CallState.GetBridgeId());
@@ -44,6 +48,31 @@ namespace Arke.Steps.HoldStep
             _call.FireStateChange(Trigger.PlaceOnHold);
             _call.Logger.Information("Hold processor fired {@Call}", call.CallState);
             _call.ProcessCallLogic();
+
+            var holdTimeout = Task.Delay(_settings.HoldTimeoutInSeconds * 1000).ContinueWith((status) =>
+            {
+                if (_call.CallState.Bridge.Id == _holdingBridge.Id)
+                    HoldTimeoutCallBack();
+                else
+                {
+                    // we're not on hold anymore, so we can just remove our events and continue
+                    _call.SipApiClient.OnPromptPlaybackFinishedEvent -= AriClient_OnPlaybackFinishedEvent;
+                    _call.OnWorkflowStep -= OnWorkflowStep;
+                    _call.SipApiClient.OnLineHangupEvent -= SipApiClientOnOnLineHangupEvent;
+                }
+            });
+        }
+
+        private void HoldTimeoutCallBack()
+        {
+            // we somehow got stuck in the holding bridge. Lets move on.
+            // we're not on hold anymore, so we can just remove our events and continue
+            _call.Logger.Warning("On Hold Timeout for call {@Call}", _call.CallState);
+            _call.SipApiClient.OnPromptPlaybackFinishedEvent -= AriClient_OnPlaybackFinishedEvent;
+            _call.OnWorkflowStep -= OnWorkflowStep;
+            _call.SipApiClient.OnLineHangupEvent -= SipApiClientOnOnLineHangupEvent;
+            _call.AddStepToProcessQueue(_timeoutStep);
+            _call.FireStateChange(Trigger.NextCallFlowStep);
         }
 
         private void SipApiClientOnOnLineHangupEvent(ISipApiClient sender, LineHangupEvent e)
